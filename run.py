6a2b233a598f5a0b85acfa787d07f4e5f6f32ac5
MajorCodingMistakesWereMade <139285976+MajorCodingMistakesWereMade@users.noreply.github.com>
2025-08-08 10:20:07 -0700

    god am i really doing this

--- run.py
+++ run.py
@@ -1,1 +1,23 @@
-from maad import sound, util
+# from maad import sound, util
+import os
+from pathlib import Path
+import json
+
+global voicebank
+global ready
+global voicebank_path
+
+ready = False
+while not ready:
+    voicebank = input("What voicebank do you want to use?")
+    script_path = Path(__file__)
+    script_directory = script_path.parent
+    voicebank_path = script_directory / "voicebanks" / voicebank
+    if not voicebank_path.exists():
+        print(f"Voicebank '{voicebank}' does not exist in the 'voicebanks' directory.")
+    else:
+        with open(voicebank_path / "Resources" / "Desc" / "Desc.json") as f:
+        json.load()
+        print(f"Using voicebank: {}")
+        ready = True
+    
